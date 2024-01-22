7f874084c9981705192e28287aa8c37e4afa8e89
May Phoo Pyae San <sakuramay1210@gmail.com>
2024-01-22 14:56:23 +0630

    fixed config file

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -7,7 +7,7 @@ import "@/styles/globals.css";
 import Router from "next/router";
 import NProgress from "nprogress";
 import "nprogress/nprogress.css";
-import Layout from "@/components/Layout";
+import Layout from "@components/Layout";
 
 Router.events.on("routeChangeStart", () => NProgress.start());
 Router.events.on("routeChangeComplete", () => NProgress.done());
